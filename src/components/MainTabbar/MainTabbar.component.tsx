6ef1af9719917851eee65e03bbc72f0b64204898
mimjs <mikhailmateevskiy@gmail.com>
2023-09-22 00:28:31 +0300

    add functional for tabbar

--- src/components/MainTabbar/MainTabbar.component.tsx
+++ src/components/MainTabbar/MainTabbar.component.tsx
@@ -2,30 +2,45 @@ import { FC } from "react";
 
 import { Tabbar, TabbarItem } from "@vkontakte/vkui";
 
-import { Icon24DollarCircleOutline, Icon24HomeOutline, Icon24MoreHorizontal } from "@vkontakte/icons";
+import {
+  Icon24DollarCircleOutline,
+  Icon24HomeOutline,
+  Icon24MoreHorizontal,
+} from "@vkontakte/icons";
 
-import { PATHS_SUB } from "../../router/router.config";
+import { PATHS_SUB, subPathsKeyNames } from "../../router/router.config";
 
 import { MainTabbarProps } from "./MainTabbar.props";
+import { useNavigate } from "react-router-dom";
+import { createNavigationPath } from "../../router/router.utils";
 
 export const MainTabbar: FC<MainTabbarProps> = ({ activeTabbar }) => {
+  const navigation = useNavigate();
+
+  const selectSubView = (viewName: subPathsKeyNames) => {
+    navigation(createNavigationPath("MAIN", viewName));
+  };
+
   return (
     <Tabbar>
       <TabbarItem
         text="Главная"
         selected={activeTabbar === PATHS_SUB.MAIN.HOME}
+        onClick={() => selectSubView("HOME")}
       >
         <Icon24HomeOutline />
       </TabbarItem>
       <TabbarItem
         text="Платежи"
         selected={activeTabbar === PATHS_SUB.MAIN.PAYMENTS}
+        onClick={() => selectSubView("PAYMENTS")}
       >
         <Icon24DollarCircleOutline />
       </TabbarItem>
       <TabbarItem
         text="Еще"
         selected={activeTabbar === PATHS_SUB.MAIN.MENU}
+        onClick={() => selectSubView("MENU")}
       >
         <Icon24MoreHorizontal />
       </TabbarItem>
